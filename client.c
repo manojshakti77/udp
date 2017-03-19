ee44afe0ae487b0f840c5e6f110f853b1c17d31d
= <=>
2017-03-19 03:48:01 -0700

    Final code comiting with updated cmd line arguments except comments

--- client.c
+++ client.c
@@ -1,101 +1,109 @@
 /************* UDP CLIENT CODE *******************/
+#include"header.h"
 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include<arpa/inet.h>
+int main(int argc,char **argv)
+{
+	int clientSocket, portNum, nBytes;
+	struct sockaddr_in serverAddr;
+  	struct timeval tv;
+  	socklen_t addr_size;
+  	FILE *fd;
+  	int seq_num = 0;
+  	struct st s1;
+	int retval;
 
-#define REG 0x01
-#define CONF 0x02
-#define REQ 0x03
-#define REPLY 0x04
-#define ERROR 0x05
+	if(argc < 4)
+	{
+		printf("Usage : ./cli port_number ip_address username password\n");
+		return 0;
+	}
+  	/*Create UDP socket*/
+  	clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+	if(clientSocket < 0)
+	{
+		perror("ClientSocket");
+		return 0;
+	}
 
-int main(){
-  int clientSocket, portNum, nBytes,i;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  struct timeval tv;
-  socklen_t addr_size;
-  int sfd;
-  int seq_num = 0;
-  struct st
-  {
-    short int pkt_type;
-    short int seq_num;
-    long port_no;
-    char name[10];
-    char data[10];
-  };
+	/*Configure settings in address struct*/
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(atoi(argv[1]));
+	serverAddr.sin_addr.s_addr = inet_addr(argv[2]);
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-
-  /*Create UDP socket*/
-  clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
-
-  /*Configure settings in address struct*/
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(7891);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-
-  /*Initialize size variable to be used later on*/
-  addr_size = sizeof serverAddr;
+	/*Initialize size variable to be used later on*/
+	addr_size = sizeof serverAddr;
 	
-  tv.tv_sec = 5;
-  tv.tv_usec = 0;
-  setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&tv, sizeof(struct timeval));
+	tv.tv_sec = MAX_SEC;
+	tv.tv_usec = MAX_USEC;
+	if((setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&tv, sizeof(struct timeval))) < 0)
+	{
+		perror("setsockopt");
+		return 0;
+	}
 
-  struct st s1;
-  s1.pkt_type = REG;
-  s1.seq_num = seq_num;
-  s1.port_no = 3000;
-  strcpy(s1.name,"CLIENT3");
-  strcpy(s1.data,"PASS");
+	s1.pkt_type = REG;
+	s1.seq_num = seq_num;
+	s1.port_no = 3000;
+	strcpy(s1.name,argv[3]);
+	strcpy(s1.data,argv[4]);
 
-  //write(clientSocket,&s1,sizeof(s1));
-  sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size);
-  perror("write");
+	if((sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size)) < 0)
+	{
+		perror("sendto");
+		return 0;
+	}
 
-	sfd = open("manoj.txt",O_RDONLY);
-  while(1)
-  {
+	/*Opening the file whose data need to be sent to the SERVER*/
+	fd = fopen("manoj.txt","r");
+	if(fd == NULL)
+	{
+		perror("fopen");
+		return 0;
+	}
+	while(1)
+	{
 	/*Receive message from server*/
     nBytes = recvfrom(clientSocket,&s1,sizeof(s1),0,NULL, NULL);
     if((nBytes < 0) || (s1.seq_num != seq_num))
     {
     	perror("recvfrom");
-  	sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size);
-	continue;	
+  		sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size);
+		continue;	
     }
-  /*---- Print the received message ----*/
-  printf("%hd---%ld---%s-----%s\n",s1.pkt_type,s1.port_no,s1.name,s1.data);
-  switch(s1.pkt_type)
-  {
+  	switch(s1.pkt_type)
+  	{
     case CONF : printf("CONNECTION CONFIRMED\n");
-        printf("Enter the data to be sent....:");
-//        scanf("%s",s1.data);
-//		strcpy(s1.data,"MANOJ");
-	//sprintf(s1.data,"%s","1MANOJ.txt");
-        s1.pkt_type = REQ;
-    	sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size);
-        //write(clientSocket,&s1,sizeof(s1));
-        perror("write");
-        break;
-    case ERROR : printf("CONNECTION ERROR\n");
-		  return 0;
-        	break;
-    case REPLY : printf("REPLY FROM SERVER....%s\n",s1.data);
-		  fgets(s1.data,sizeof(data),fd);
-		  seq_num = seq_num ^ 0x011;
-		  s1.seq_num = seq_num;
-		 sleep(2);
-    		 sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size);
-        	 break;
+				sprintf(s1.data,"%s","manoj.txt");
+		  		seq_num = ((seq_num)^(0x01));
+        		s1.pkt_type = REQ;
+		  		s1.seq_num = seq_num;
+    			if((sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size)) < 0)
+				{
+					perror("sendto::CONF");
+					return 0;
+				}
+        		break;
+    case ERROR: printf("CONNECTION ERROR\n");
+				return 0;
+    case REPLY: printf("REPLY FROM SERVER....%s\n",s1.data);
+		  		if(fgets(s1.data,sizeof(s1.data),fd))
+				{
+		  		seq_num = ((seq_num)^(0x01));
+				s1.pkt_type = REQ;
+		  		s1.seq_num = seq_num;
+    		 	if((sendto(clientSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverAddr,addr_size)) < 0)
+				{
+					perror("sento::REPLY");
+					return 0;
+				}
+				}
+				else
+					return 0;
+        	 	break;
     default : printf("Invalid packet\n");
               break;
-  }
-  }
-
+	}
+	}
   return 0;
 }
--- server.c
+++ server.c
@@ -1,45 +1,79 @@
-#include<stdio.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<string.h>
-#include<stdlib.h>
-#include<arpa/inet.h>
-#include<ctype.h>
+#include"header.h"
 
-#define REG		0x01
-#define CONF 	0x02
-#define REQ	 	0x03
-#define REPLY 	0x04
-#define ERROR 	0x05
+struct database **ptr;
+struct database base;
+int size;
 
-struct database
+int database_create(void)
 {
-    char name[10];
-    char password[10];
-};
+	int i,n,j,k;
+	char buf[50];
 
-int main()
+	int size;
+
+	/*Reading the password database and forming a linkedlist*/
+    FILE *fd = fopen("db.txt","r");
+	if(fd == NULL)
+	{
+		perror("fopen");
+		return 0;
+	}
+	fgets(buf,sizeof(buf),fd);
+    size = atoi(buf);
+	j = 0;
+	k = 0;
+    ptr = (struct database **)malloc(size * sizeof(struct database *));
+
+    for(i = 0;i<size;i++)
+	{
+        ptr[i] = (struct database *)malloc(sizeof(base));
+		if(ptr[i] == NULL)
+		{
+    		perror("malloc");
+			return 0;
+		}
+	}
+
+	while(fgets(buf,sizeof(buf),fd))
+    {
+     for(i=0;buf[i] != ' ';i++);
+     strncpy(ptr[j]->name,buf,i);
+     i++;
+     for(k=0;buf[i+k] != '\n';k++);
+     strncpy(ptr[j++]->password,&buf[i],k);
+    }
+    fclose(fd);
+	return size;
+}
+int main(int argc,char **argv)
 {
-  int udpSocket, nBytes;
-  char buf[64];
-  struct sockaddr_in serverAddr, clientAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size, client_addr_size;
-  int i,n,size,j,k;
-  struct database **ptr;
-   struct database base;
-	
-  struct st
-  {
-       short int pkt_type;
-       long port_no;
-       char name[10];
-       char data[10];
-  };
+	int udpSocket, nBytes;
+	struct sockaddr_in serverAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size, client_addr_size;
+	int i,j;
+	struct client_info client_info;//To store the client information once connection confirmed 
+	int seq_num = 1;
+	int index = 0;
 	struct st s1;
+	int retval;
+	int size;
+	int fd;
+
+	if(argc < 2)
+	{
+		printf("Usage : ./ser port_number ip_address\n");
+		return 0;
+	}
+	
+	size = database_create();
+	if(size == 0)
+		return 0;
+		
+#if 0
+	/*Reading the password database and forming a linkedlist*/
 	FILE *fd = fopen("db.txt","r");
     perror("open");
-
 	fgets(buf,sizeof(buf),fd);
     size = atoi(buf);
     ptr = (struct database **)malloc(size * sizeof(struct database *));
@@ -55,26 +89,46 @@ int main()
      for(k=0;buf[i+k] != '\n';k++);
      strncpy(ptr[j++]->password,&buf[i],k);
     }
+	fclose(fd);
+#endif
+	/*Create UDP socket*/
+	udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+	if(udpSocket < 0)
+	{
+		perror("socket");
+		return 0;
+	}
 
-  /*Create UDP socket*/
-  udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+	/*Configure settings in address struct*/
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(atoi(argv[1]));
+	serverAddr.sin_addr.s_addr = inet_addr(argv[2]);
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-  /*Configure settings in address struct*/
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(7891);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+	/*Bind socket with address struct*/
+	if(bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
+	{
+		perror("bind");
+		return 0;
+	}
+	
 
-  /*Bind socket with address struct*/
-  bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	/*Initialize size variable to be used later on*/
+	addr_size = sizeof serverStorage;
 
-  /*Initialize size variable to be used later on*/
-  addr_size = sizeof serverStorage;
-
-  while(1){
+	while(1){
     /* Try to receive any incoming UDP datagram. Address and port of 
       requesting client will be stored on serverStorage variable */
     nBytes = recvfrom(udpSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverStorage, &addr_size);
+	if(seq_num == s1.seq_num)
+	{
+		printf("Duplicate DATA\n");
+		s1.pkt_type = REPLY;
+		memset(s1.data,'\0',sizeof(s1.data));
+		sendto(udpSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverStorage,addr_size);
+		continue;
+	}
+	seq_num = s1.seq_num;
 
 	if(nBytes < 0)
 	{
@@ -84,14 +138,17 @@ int main()
      switch(s1.pkt_type)
      {
         case REG:printf("CLIENT REGISTER.....%s  %s\n",s1.name,s1.data);
+				/*Logic for the AUTHENTICATION*/
                  for(i=0;i<size;i++)
                  {
                    printf("%s\n",ptr[i]->name);
                    if(!strcmp(s1.name,ptr[i]->name))
                    {
-                    printf("%s     %s",ptr[i]->password,s1.data);
+                    printf("%s     %s\n",ptr[i]->password,s1.data);
                       if(!strcmp(ptr[i]->password,s1.data))
                       {
+						client_info.port_no = s1.port_no;
+						strcpy(client_info.name,s1.name);
                         s1.pkt_type = CONF;
                         break;
                       }
@@ -99,29 +156,54 @@ int main()
                  }
                  if(i == size)
                     s1.pkt_type = ERROR;
-                // write(sock,&s1,sizeof(s1));
-    		   	sendto(udpSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverStorage,addr_size);
-                 perror("write");
-                 if(i == size)
+				 sendto(udpSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverStorage,addr_size);
+				for(j = 0;j<size;j++)
+				{
+    				free(ptr[j]);
+				//	perror("free");
+				}
+				free(ptr);
+			//	perror("free---");
+                if(i == size)
                     return 0;
-
                  break;
-		case REQ : printf("REQUEST DATA FROM CLIENT.....%s\n",s1.data);
-               i = strlen(s1.data);
-               sprintf(s1.data,"%d",i);
-               printf("Data sent....%s\n",s1.data);
-               s1.pkt_type = REPLY;
-    		   sendto(udpSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverStorage,addr_size);
-               //write(sock,&s1,sizeof(s1));
-               perror("write");
-               break;
-
+		case REQ:if((s1.port_no == client_info.port_no) && (strcmp(s1.name,client_info.name)))
+				 {
+					s1.pkt_type = ERROR;
+    		   		sendto(udpSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverStorage,addr_size);
+					break;
+				 }
+				 if(index == 0)
+				 {
+					printf("Name of the file.....%s\n",s1.data);
+					if((fd = open(s1.data,O_CREAT|O_WRONLY)) < 0)
+					{
+						perror("open::file");
+						return 0;
+					}
+				 }
+				 else
+				 {	
+					printf("Data:%s",s1.data);
+					if(write(fd,s1.data,strlen(s1.data)) < 0)
+					{
+						perror("write");
+						return 0;
+					}
+				 }
+               	 s1.pkt_type = REPLY;
+				 memset(s1.data,'\0',sizeof(s1.data));
+				 if((++index) == 5)
+					 continue;
+    		   	 if(sendto(udpSocket,&s1,sizeof(s1),0,(struct sockaddr *)&serverStorage,addr_size) < 0)
+				 {
+				 	perror("sendto::REQ");
+					return 0;
+				 }
+               	 break;
         default: printf("Unknown\n");
                  break;
      }
-
-    /*Send uppercase message back to client, using serverStorage as the address*/
-//	while(1);
   }
 
   return 0;
